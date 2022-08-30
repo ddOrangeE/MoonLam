07c141119055f9522267c72a998f542d26d51184
DESKTOP-KHVJGSC\user <0414soyeon@gmail.com>
2022-08-30 16:31:53 +0900

    [김소연] - MoonLam project 최초 생성

---
+++ src/test/java/com/soyaa/moonlam/MoonLamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soyaa.moonlam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonLamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
